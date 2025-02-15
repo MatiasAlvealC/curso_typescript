8df73afbd5a555913fc6d445e7b4f156d79c3524
Matias Alveal <matiasalvealc@gmail.com>
2025-02-15 14:48:46 -0300

    proyecto server final

--- api/src/database.ts
+++ api/src/database.ts
@@ -3,12 +3,12 @@ import { Club, Clubs, Match, Matches } from "./types";
 const clubs: Clubs = require("../data/clubs.json")
 const resultados: Matches = require("../data/resultados.json")
 
-function getClub(code: string): Club {
+export function getClub(code: string): string {
   const club =  clubs.clubs.find(club => club.code === code)
   if (!club) {
-    throw new Error("No existe este equipo")
+    throw new Error(`No existe el equipo: ${code}`)
   }
-  return club
+  return club.name
 }
 
 export function getResultados(eq1: string, eq2: string): Match {
@@ -16,7 +16,7 @@ export function getResultados(eq1: string, eq2: string): Match {
     return eq1 === match.team1 && eq2 === match.team2
   })
   if (!match) {
-    throw new Error("No existe este partido este año")
+    throw new Error(`No existe un partido entre ${eq1} y ${eq2} este año`)
   }
   return match
 }
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,8 +1,31 @@
 import express from "express"
+import {getClub, getResultados} from "./database"
+import { match } from "assert"
+import { markAsUncloneable } from "worker_threads"
+
+
 export default function createServer() {
     const app = express()
     app.get('/resultados/:equipo1/:equipo2',(req, res) =>{
-      res.send(`Resultados del ${req.params.equipo1} vs ${req.params.equipo2}`)
+      try {
+        let team1 = getClub(req.params.equipo1)
+        let team2 = getClub(req.params.equipo2)
+        let match = getResultados(team1, team2)
+        if (match.score) {
+          res.json ({
+            date: match.date,
+            result: `${team1} ${match.score.ft[0]} -${match.score.ft[1]} ${team2}`,
+          })
+        } else {
+          res.json({
+            date: match.date,
+          })
+        }
+      } catch (e: any) {
+        res.status(400).json({
+          error: e.message
+        })
+      }
     })
 
     return app
