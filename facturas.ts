4e011daf024a6cce606fdfbf24f43778824d816d
Matias Alveal <matiasalvealc@gmail.com>
2025-02-14 23:11:39 -0300

    import/export default

--- facturas.ts
+++ facturas.ts
@@ -1,6 +1,7 @@
 import {Producto, sumarTotal} from "./almacen"
-
+import IVA from "./impuestos"
 
 function procesarPedido( p: Producto[]) {
   console.log("Procesando...")
+  console.log(`El IVA es ${IVA(10.99)}`)
 }
---
+++ impuestos.ts
@@ -0,0 +1,3 @@
+export default function IVA (precio: number) {
+    return precio *1.21
+}
