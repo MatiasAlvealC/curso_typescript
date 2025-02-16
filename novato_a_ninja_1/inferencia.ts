d4370ccf9d86a0692e4d1a11e0b8c2b38ef69552
Matias Alveal <matiasalvealc@gmail.com>
2025-02-16 17:14:27 -0300

    curso de novato a ninja 1/ inferencia

---
+++ novato_a_ninja_1/inferencia.ts
@@ -0,0 +1,12 @@
+// infernecia
+
+// como a y b infiere que son number sin decirle nadda
+const a = 1
+const b = 2
+const c = a + b // c tambien será number
+
+let cadenadetexto = 'hola'
+
+cadenadetexto.toLocaleLowerCase()
+
+let obj: any  = { x : 0};
