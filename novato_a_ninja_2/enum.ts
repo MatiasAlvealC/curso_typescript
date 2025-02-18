cc17bdbc36cc74e9a57aefe1b37f79da5aae907f
Matias Alveal <matiasalvealc@gmail.com>
2025-02-18 20:21:27 -0300

    Aserciones de tipos

--- novato_a_ninja_2/enum.ts
+++ novato_a_ninja_2/enum.ts
@@ -36,3 +36,26 @@ function mostrarMensaje (tipoDeError:ERROR_TYPES) {
     console.log('No tienes permisos para acceder')
   }
 }
+
+
+// Aserciones de tipo
+
+// typescriot no sabe que estas recuperando un elemento canvas
+const canvas = document.getElementById('canvas') // as HTMLCanvasElement
+
+// esto devolvera:
+// null o HTMLelement
+// y nosotros necesitamos HTMLCanvasElemente
+// para eso se lo señalamos con as
+// pero tambien lo podemos hacer asi
+if (canvas !== null && canvas instanceof HTMLCanvasElement) {
+  const ctx = canvas.getContext('2d')
+}
+
+// y esto es inferencia, typescript se da cuenta que dentro del if
+// ya solo el canv va a poder ser un HTMLCanvasElement
+
+/*
+typeof -> para tipos
+instaceof -> para instancias
+*/
