f1ad4378112f477615c978b35d40277f32073b16
Matias Alveal <matiasalvealc@gmail.com>
2025-02-18 20:06:43 -0300

    enum

---
+++ novato_a_ninja_2/enum.ts
@@ -0,0 +1,38 @@
+// enums
+/*
+Asi se haria en javaScript
+const ERROR_TYPES = {
+  NOT_FOUND: 'notFound',
+  UNAUTHORIZED: 'unauthorized',
+  FORBIDDEN: 'forbidden'
+}
+
+function mostrarMensaje (tipoDeError) {
+  if (tipoDeError === ERROR_TYPES.NOT_FOUND) {
+    console.log('No se encuentra el recurso')
+  } else if (tipoDeError === ERROR_TYPES.UNAUTHORIZED) {
+    console.log('No tienes permisos para acceder')
+  } else if (tipoDeError === ERROR_TYPES.FORBIDDEN) {
+    console.log('No tienes permisos para acceder')
+  }
+}
+
+pero en TypeScript se haria asi:
+*/
+
+
+enum ERROR_TYPES  {
+  NOT_FOUND,
+  UNAUTHORIZED,
+  FORBIDDEN
+}
+
+function mostrarMensaje (tipoDeError:ERROR_TYPES) {
+  if (tipoDeError === ERROR_TYPES.NOT_FOUND) {
+    console.log('No se encuentra el recurso')
+  } else if (tipoDeError === ERROR_TYPES.UNAUTHORIZED) {
+    console.log('No tienes permisos para acceder')
+  } else if (tipoDeError === ERROR_TYPES.FORBIDDEN) {
+    console.log('No tienes permisos para acceder')
+  }
+}
