1285deb6e33dfa936e14407f0658b565e363e5e9
Matias Alveal <matiasalvealc@gmail.com>
2025-02-17 21:33:16 -0300

    Union  y  interseccion type

--- novato_a_ninja_1/types.ts
+++ novato_a_ninja_1/types.ts
@@ -1,5 +1,5 @@
 // Types Alias
-
+/*
 type HeroId = `${string}-${string}-${string}-${string}-${string}`
 type Hero = {
   readonly id?: HeroId, // redonly, solo de lenctura
@@ -23,9 +23,49 @@ function createHero(hero: Hero): Hero {
 const thor = createHero({name: 'Thor',age: 1500})
 console.log(thor.isActive) //-> true
 
-
+*/
 // template union types
 
 type HexadecimalColor = `#${string}`
 //const color: HexadecimalColor = '0033ff' //hexadecimales
 const color2: HexadecimalColor = '#0033ff' //hexadecimales
+
+
+// Union type y  interseccion type
+// | tipo de union, es un o
+let annn: number | string // ann puede ser number o string
+
+type HeroPowerScale = 'local' | 'planetary' | 'galactic' | 'universal' | 'multiversal'
+type HeroId = `${string}-${string}-${string}-${string}-${string}`
+
+type HeroBasicInfo = {
+  name: string,
+  age: number,
+}
+type HeroProperties = {
+  readonly id?: HeroId, // redonly, solo de lenctura
+  isActive?: boolean // optional properties
+  powerScale?: HeroPowerScale
+}
+
+type Hero = HeroBasicInfo & HeroProperties  // interseccion type
+
+let hero: Hero = {
+  name: 'thor',
+  age: 1500
+}
+
+function createHero(input: HeroBasicInfo): Hero {
+  const  {name, age } = input
+  return {
+    id: crypto.randomUUID(),
+    name, age, 
+    isActive: true}
+}
+
+const thor = createHero({name: 'Thor',age: 1500})
+console.log(thor.isActive) //-> true
+
+
+// Type index
+
